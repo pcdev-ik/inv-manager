f2009569277b212ba6feb986cbbce34343dc7bc9
pcdev-ik <ikaisara@mail.pima.edu>
2026-09-03 18:11:06 -0700

    Adds banner and tests comment visibility

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+I haven't made a program since a scripting class nearly a year ago (✿◕_◕)
+I miss PyCharm... ( 〒▽〒)
+"""
+print(r""".-. .-. .----..-..-.   .-.
+| | | |{ {__  | ||  `.'  |
+\ \_/ /.-._} }| || |\ /| |
+ `---' `----' `-'`-' ` `-'
+ Very Simple Inventory Manager""")
